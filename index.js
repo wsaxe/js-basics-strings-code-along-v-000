bfd523e08caa7c17b578b960eb5ff5f979ddf0c6
William Saxe <geosaxe@gmail.com>
2017-10-15 18:55:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,4 @@
 const currentUser = 'Jamaican Mecrazy' ;
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!` ;
 const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`
+const shortGreeting = `Welcome, ${currentUser[0]}!`
