9a0a0c00780923c5371eaa46b6187d89c3d9dc4a
William Saxe <geosaxe@gmail.com>
2017-10-15 18:50:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 // Write your code in this file!
 const currentUser = 'Jamaican Mecrazy' ;
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!` ;
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser}!`
